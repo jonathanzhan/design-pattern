808df56e43d71646538e3abb7f5427c625b2ae53
jonathan <jonathan.zhan.chang@gmail.com>
2019-08-20 11:21:30 +0800

    建造者模式

---
+++ src/main/java/com/workfocus/dp/creation/builder/Product.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright  2014-2019 jonathan.zhan.chang@gmail.com(Jonathan)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.workfocus.dp.creation.builder;
+
+/**
+ * 产品角色
+ *
+ * @author Jonathan
+ * @version 1.0.0
+ * @date 2019/8/20 10:35
+ * @since 1.0.0+
+ */
+public class Product {
+    //以下定义产品的零部件
+    /**
+     * 产品的零部件1
+     */
+    private String part1;
+
+    /**
+     * 产品的零部件2
+     */
+    private String part2;
+
+    /**
+     * 产品的零部件2
+     */
+    private String part3;
+
+
+    public String getPart1() {
+        return part1;
+    }
+
+    public void setPart1(String part1) {
+        this.part1 = part1;
+    }
+
+    public String getPart2() {
+        return part2;
+    }
+
+    public void setPart2(String part2) {
+        this.part2 = part2;
+    }
+
+    public String getPart3() {
+        return part3;
+    }
+
+    public void setPart3(String part3) {
+        this.part3 = part3;
+    }
+}
