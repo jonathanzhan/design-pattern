3b118a1e0769b78127ed806b95ff011db5b15be8
Jonathan <zhan.chang@outlook.com>
2019-08-29 17:37:57 +0800

    更新一波copyright

--- src/main/java/com/clexel/dp/creation/builder/ConcreteBuilder.java
+++ src/main/java/com/clexel/dp/creation/builder/ConcreteBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  2014-2019 jonathan.zhan.chang@gmail.com(Jonathan)
+ * Copyright  2014-2019 zhan.chang@outlook.com(Jonathan)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
